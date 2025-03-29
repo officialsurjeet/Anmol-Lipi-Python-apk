5040573a476478f14e02140e7c849a816835a53a
officialsurjeet <44050893+officialsurjeet@users.noreply.github.com>
2025-03-29 20:36:10 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,10 @@
+# src/__init__.py
 
+"""
+Package for Python Android APK Project.
+
+This package includes the modules required for reading an Excel file
+and displaying the font information of each character.
+"""
+
+__version__ = "0.1"
