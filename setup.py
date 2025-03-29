692c2a8163fbca405a2e2e57059e41e19a95d2f2
officialsurjeet <44050893+officialsurjeet@users.noreply.github.com>
2025-03-29 20:33:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='python-android-apk',
+    version='0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'openpyxl',
+        'kivy'
+    ],
+)
